1f218bc33598ae89afbbff8b4e3b4e744ea1572e
ManuDevPro <jacome.style@gmail.com>
2026-08-31 19:18:44 +0200

    Avanzado el capitulo de funciones

---
+++ 2- ConceptosDeProgramacion/PracticaFunciones.js
@@ -0,0 +1,20 @@
+
+
+//Imprimir los numero del 1 al 5 usando bucle FOR
+
+let i = 1;
+for (i=1; i<=5; i++){
+    console.log(i);
+}
+
+
+
+
+//Imprimir los numero del 1 al 5 usando bucle WHILE
+
+a = 1;
+console.log('<br>');
+while(a <= 5){
+    console.log(a);
+    a++;
+}
